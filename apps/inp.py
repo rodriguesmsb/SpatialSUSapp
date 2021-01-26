a039e0baf5c7966ec69edd076af17d7e5fa8260d
rodriguemsb <rodriguesmsb@gmail.com>
2021-01-26 16:42:14 -0400

    start input

--- apps/inp.py
+++ apps/inp.py
@@ -16,10 +16,24 @@ layout = html.Div(
             children = [
                 html.P("SpatialSUSapp", className = "header-title"),
                 html.P("Um aplicação em dash para a visualização de dados agregados", className = "home-text")
-
             ],
             className = "header"
+        ),
+        html.Section(
+            id = "painel",
+            children = [
+                html.Div(
+                    id = "shapefile",
+                    children = [],
+                    className = "col"
+        ),
+                html.Div(id = "up_data", children = [], className = "col"),
+                html.Div(id = "sel_an", children = [], className = "col"),
+            ],
+            className = "section"
         )
+        
+
     ],
     className = "body"
 )
