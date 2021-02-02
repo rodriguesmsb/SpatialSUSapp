ab9fa73b1b94b27f0e1a8fc97e6c893e1b3c4cc6
rodriguesmsb <rodriguesmsb@gmail.com>
2021-02-02 14:12:07 -0400

    redo template

--- apps/home.py
+++ apps/home.py
@@ -11,20 +11,20 @@ import dash_html_components as html
 
 
 layout = html.Div(
-    children = [
-        html.Div(
-            children = [html.P("VizSUSapp", className = "h1"),
-                        html.P("Aplicativo para visualização de dados agregados", className = "h2")],
-            className = "header"),
-        html.Div(
-            children = [html.P("NavBar", className = "text")],
-            className = "nav-bar"),
-        html.Div(
-            children = [html.P("MainBody", className = "text")],
-            className = "main-body"
-        )
+    # children = [
+    #     html.Div(
+    #         children = [html.P("VizSUSapp", className = "h1"),
+    #                     html.P("Aplicativo para visualização de dados agregados", className = "h2")],
+    #         className = "header"),
+    #     html.Div(
+    #         children = [html.P("NavBar", className = "text")],
+    #         className = "nav-bar"),
+    #     html.Div(
+    #         children = [html.P("MainBody", className = "text")],
+    #         className = "main-body"
+    #     )
 
-    ]   
+    # ]   
 )
 
 
