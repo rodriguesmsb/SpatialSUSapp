bef1e7afcfd859b49063e2ba150f75492a52031b
rodriguesmsb <rodriguesmsb@gmail.com>
2021-02-16 15:44:30 -0400

    style info

--- index.py
+++ index.py
@@ -12,7 +12,13 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 from app import app
-from apps import viz
+from apps import spatio_temporal
+import pandas as pd
+
+
+
+
+
 
 
 ###Add code to use external css
@@ -23,9 +29,6 @@ external_stylesheets = ["https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7
                         "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap"])
 app.title = "Data visualization"
 
-import pandas as pd
-data = pd.read_csv("data/data.csv")
-print(data.head())
 
 app.layout = html.Div([
     dcc.Location(id = 'url', refresh = False),
@@ -41,7 +44,7 @@ def display_page(pathname):
     elif pathname == "#":
         return 404
     else:
-        return viz.layout
+        return spatio_temporal.layout
 
 if __name__ == '__main__':
     app.run_server(debug = True)
